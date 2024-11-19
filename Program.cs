6ac190a1537963a38728c4e22c37a5e4bfc04cea
Peter Andrén <piddah80@gmail.com>
2024-11-19 14:04:20 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(Calculate(12, "/", 2));
+
+double Calculate(double nr1, string operation, double nr2) =>
+    operation switch
+    {
+        "+" => nr1 + nr2,
+        "-" => nr1 - nr2,
+        "*" => nr1 * nr2,
+        "/" => nr1 / nr2,
+        "" => throw new ArgumentException(),
+        null => throw new ArgumentNullException(),
+        _ => throw new ArgumentOutOfRangeException()
+    };
